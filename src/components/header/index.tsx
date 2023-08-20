e59afc708441925077acd4e4d27af7c5febae875
mmrakt <mimuraakkun3@gmail.com>
2023-08-20 21:48:31 +0900

    add clsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,51 +1,61 @@
 import React, { useState } from "react";
 import s from "./header.module.scss";
+import { clsx } from "clsx";
 
 const popupMenuItems = [
   {
     image: "scrap.png",
     text: "スクラップの管理",
     link: "",
+    endSection: false,
   },
   {
     image: "post.png",
     text: "記事の管理",
     link: "",
+    endSection: false,
   },
   {
     image: "book_mini.png",
     text: "本の管理",
     link: "",
+    endSection: false,
   },
   {
     image: "pull_request.png",
     text: "GitHubからのデプロイ",
     link: "",
+    endSection: false,
   },
   {
     image: "like.png",
     text: "いいねした投稿",
     link: "",
+    endSection: false,
   },
   {
     image: "bookmark.png",
     text: "読んでいる本",
     link: "",
+    endSection: false,
   },
   {
     image: "yen.png",
     text: "収益ダッシュボード",
     link: "",
+    endSection: false,
   },
   {
     image: "setting.png",
     text: "アカウント設定",
     link: "",
+    endSection: true,
   },
   {
     image: "logout.png",
     text: "ログアウト",
     link: "",
+    endSection: false,
   },
 ];
 
@@ -58,7 +68,7 @@ const Header: React.FC = () => {
         <div className={s.container}>
           <div className={s.top}>
             <div className={s.title}>
-              <h1>Title</h1>
+              <h1>Zenn Clone</h1>
             </div>
             <div className={s.menu}>
               <button className={s.search_icon}>
@@ -84,7 +94,14 @@ const Header: React.FC = () => {
                   <div className={s.popup_body}>
                     <ul className={s.popup_list}>
                       {popupMenuItems.map((item) => (
-                        <li className={s.popup_item}>
+                        <li
+                          key={item.image}
+                          className={clsx(
+                            item.endSection
+                              ? s["popup_item--bottomLine"]
+                              : s.popup_item
+                          )}
+                        >
                           <a href={item.link}>
                             <img src={`/images/${item.image}`} alt="" />
                             <span>{item.text}</span>
